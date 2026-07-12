a1088656301895ac7b6b7d7ce9a5247860534a65
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-07-12 15:06:08 +0630

    Render languages section

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,27 @@
-import { useState } from "react";
+import { useState } from "react"
+import{languages} from "./languages"
 
 export default function Main(){
+
+    const [lang , setLang] =useState(()=>languagesArr())
+
+    function languagesArr(){
+        const langArray= languages.map(n=>{
+            return {
+                n : n.name,
+                b : n.backgroundColor,
+                c : n.color
+            }
+        })
+
+        return langArray
+    }
+
+    const RenderLanguages =lang.map(n=>{
+        return  <p style={{backgroundColor :n.b,
+                   color: n.c}
+                  }>{n.n}</p>
+    })
 
     return (
         <main>
@@ -8,8 +29,9 @@ export default function Main(){
             <h2>You win!</h2>
             <p>Well done! 🎉</p>
             </section>
+            <section className="languages">
+                {RenderLanguages}
+            </section>
         </main>
     )
-
-    
 }
