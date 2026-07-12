076bb9e7eb14ae880e5beab0b72aecf1361632b2
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-07-12 13:39:18 +0630

    Game status section in Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Header from './Components/Header'
+import Main  from './Components/Main'
+
 
 export default function App(){
   return (
     <>
     <Header/>
+    <Main/>
     </>
   )
 }
---
+++ src/Components/Main.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Main(){
+
+    return (
+        <main>
+            <section className="game-status">
+            <h2>You win!</h2>
+            <p>Well done! 🎉</p>
+            </section>
+        </main>
+    )
+
+    
+}
